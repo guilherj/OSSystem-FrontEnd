a8983c9677f4f5130db4cb429e5560daaa4affad
guilherj <contato.guilhermenascimento@hotmail.com>
2024-02-09 17:21:19 -0300

    :sparkles: feat: Generating HEADER component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,49 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { MatSelectModule } from '@angular/material/select';
+import { MatInputModule } from '@angular/material/input';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatListModule } from '@angular/material/list';
+import { MatCardModule } from '@angular/material/card';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './views/components/template/header/header.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    BrowserModule, 
+    AppRoutingModule, 
+    FormsModule, 
+    ReactiveFormsModule,     
+    HttpClientModule, 
+    BrowserModule, 
+    AppRoutingModule, 
+    BrowserAnimationsModule, 
+    MatToolbarModule, 
+    MatSidenavModule, 
+    MatIconModule, 
+    MatButtonModule, 
+    MatListModule, 
+    MatCardModule, 
+    MatTableModule, 
+    MatSelectModule, 
+    MatInputModule, 
+    MatDatepickerModule, 
+    MatPaginatorModule, 
+    MatSnackBarModule
   ],
   providers: [
     provideClientHydration(),
