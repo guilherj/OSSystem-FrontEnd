402c9a9b04b4139a783bd056f77eaa88e5edebf3
guilherj <contato.guilhermenascimento@hotmail.com>
2024-03-17 11:36:05 -0300

    :sparkles: feat: Finishing os-create component

--- src/app/views/components/os/os-create/os-create.component.ts
+++ src/app/views/components/os/os-create/os-create.component.ts
@@ -3,6 +3,9 @@ import { TecnicoService } from '../../../../services/tecnico.service';
 import { ClienteService } from '../../../../services/cliente.service';
 import { Tecnico } from '../../../../models/Tecnico';
 import { Cliente } from '../../../../models/Cliente';
+import { OS } from '../../../../models/os';
+import { OsService } from '../../../../services/os.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-os-create',
@@ -13,17 +16,42 @@ export class OsCreateComponent implements OnInit {
 
   selected = ''
 
+  os: OS = {
+    tecnico: '',
+    cliente: '',
+    observacoes: '',
+    status: '',
+    prioridade: ''
+  }
+
   tecnicos: Tecnico[] = []
   clientes: Cliente[] = []
 
   constructor(
     private tecnicoService: TecnicoService,
-    private clienteService: ClienteService
+    private clienteService: ClienteService,
+    private service: OsService,
+    private router: Router
   ) { }
 
   ngOnInit(): void {  
     this.listarTecnicos();
     this.listarClientes();
+  }
+
+  create(): void {
+    this.service.create(this.os).subscribe(resposta => {
+      this.service.message("OS criada com sucesso!");
+      this.router.navigate(['os'])
+      console.log()
+
+    }, err => {      
+      this.service.message(err.error.errors[0].message)      
+    })
+  }
+
+  cancel(): void {
+    this.router.navigate(['os'])
   }
 
   listarTecnicos():void  {
