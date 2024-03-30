5b9df1a6041c99d352e8d334d6d31df09fb96d2c
guilherj <contato.guilhermenascimento@hotmail.com>
2024-03-30 19:16:54 -0300

    :recycle: refactor: Changed back end URL for service deployed in the cloud of the RENDER website.

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,4 @@
 export const environment = {   
-    baseUrl: 'http://localhost:8080/api/v1'  // dev    
-    //baseUrl: 'https://os-system-curso-fullstack-valdircezar.onrender.com/api/v1' // prod
+    //baseUrl: 'http://localhost:8080/api/v1'  // dev    
+    baseUrl: 'https://os-system-curso-fullstack-valdircezar.onrender.com/api/v1' // prod
 };
